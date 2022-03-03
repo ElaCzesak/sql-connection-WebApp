9c0fbaefa2fb19ce084779a9424075e4371269fa
Ela Czesak <czesakelzbieta1311@gmail.com>
2022-03-03 15:04:26 +0100

    qcl connection

---
+++ WebApplicationSQL/WebApplicationSQL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationSQL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebApplicationSQL/WebApplicationSQL/SqlConnector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using WebApplicationSQL.Models;
+
+namespace WebApplicationSQL
+{
+    public class SqlConnector
+    {
+        public List<Car> ReadCarData()
+        {
+            string connectionString = "Data Source=DESKTOP-FCNEOFV;Initial Catalog=restDB;Integrated Security=SSPI";
+            string queryString = "SELECT ID, AutoName FROM dbo.Cars;";
+
+            List<Car> carList = new List<Car>();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    carList.Add(new Car()
+                    {
+                        ID = reader.GetFieldValue<int>(0),
+                        Name = reader.GetFieldValue<string>(1)
+                    });
+                }
+                reader.Close();
+            }
+
+            return carList;
+        }
+    }
+}
